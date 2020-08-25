33c5a955d5c0f16705da4000a77433461b5e1082
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-08-24 21:34:01 -0300

    Aula 2

---
+++ Aula_02 - Operadores/ArithmeticDemo.java
@@ -0,0 +1,33 @@
+class ArithmeticDemo {
+  public static void main(String[] args) {
+    int result = 1 + 2;
+
+    System.out.println(result);
+
+    int originalResult = result;
+
+    result = result - 1;
+
+    System.out.println(originalResult + " - 1 = " + result);
+    originalResult = result;
+
+    result = result * 2;
+
+    System.out.println(originalResult + " * 2 = " + result);
+    originalResult = result;
+
+    result = result / 2;
+
+    System.out.println(originalResult + " / 2 = " + result);
+    originalResult = result;
+
+    result = result + 8;
+
+    System.out.println(originalResult + " + 8 = " + result);
+    originalResult = result;
+
+    result = result % 7;
+
+    System.out.println(originalResult + " / 7 = " + result);
+  }
+}
