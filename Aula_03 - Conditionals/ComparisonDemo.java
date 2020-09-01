424f788fc657e20be82ebf6d850e3afb514d02e1
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-09-01 18:19:06 -0300

    Aula 3

---
+++ Aula_03 - Conditionals/ComparisonDemo.java
@@ -0,0 +1,26 @@
+class ComparisonDemo {
+  public static void main(String[] args) {
+    int value_01 = 1;
+    int value_02 = 2;
+
+    if(value_01 == value_02) {
+      System.out.println("value_01 == value_02");
+    }
+
+    if(value_01 != value_02) {
+      System.out.println("value_01 != value_02")
+    }
+
+    if(value_01 > value_02) {
+      System.out.println("value_01 > value_02")
+    }
+
+    if(value_01 < value_02) {
+      System.out.println("value_01 < value_02");
+    }
+
+    if(value_01 <= value_02) {
+      System.out.println("value_01 <= value_02");
+    }
+  }
+}
