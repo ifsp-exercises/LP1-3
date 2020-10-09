53f1ba101a1ab3e957e71faa34a1d01fec523c68
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-10-09 12:57:32 -0300

    Trocando de computador

--- TP_03/Hour.java
+++ TP_03/Hour.java
@@ -1,33 +1,53 @@
 public class Hour {
-    private int hours;
-    private int minutes;
-    private int seconds;
+  private int hours;
+  private int minutes;
+  private int seconds;
+  private final String hourTemplate = "%d:%d:%d";
 
-    public Hour() {
-        this.hours = 0;
-        this.minutes = 0;
-        this.seconds = 0;
-    }
+  public Hour() {
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+  }
 
-    public Hour(int hours, int minutes, int seconds) {
-        this.hours = hours;
-        this.minutes = minutes;
-        this.seconds = seconds;
-    }
+  public Hour(int hours, int minutes, int seconds) {
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+  }
 
-    public void SetHours(int hours) {
-        this.hours = hours;
-    }
+  public void SetHours(int hours) {
+    this.hours = hours;
+  }
 
-    public void SetMinutes(int minutes) {
-        this.minutes = minutes;
-    }
+  public void SetMinutes(int minutes) {
+    this.minutes = minutes;
+  }
 
-    public void SetSeconds(int seconds) {
-        this.seconds = seconds;
-    }
+  public void SetSeconds(int seconds) {
+    this.seconds = seconds;
+  }
 
-    public int GetHours() {
-        return this.hours;
-    }
+  public int GetHours() {
+    return this.hours;
+  }
+
+  public int GetMinutes() {
+    return this.minutes;
+  }
+
+  public int GetSeconds() {
+    return this.seconds;
+  }
+
+  public String getCompleteHour() {
+    String completeHour = String.format(
+      this.hourTemplate,
+      this.hours,
+      this.minutes,
+      this.seconds
+    );
+
+    return completeHour;
+  }
 }
---
+++ TP_03/TestHour.java
@@ -0,0 +1,8 @@
+public class TestHour {
+
+  public static void main(String[] args) {
+    Hour hour = new Hour(10, 20, 20);
+
+    System.out.println(hour.getCompleteHour());
+  }
+}
